284ad98b99304e9ed3322f14ef5f53750a3e6d3e
Willian Michel <willian.tessarolo@itmapps.com.br>
2025-07-19 10:13:12 -0300

    feat: Migrations para criação da tabela de projeto e tabela auxiliar project_user. Criação do primeiro seeder para usuário.

---
+++ project-manager/database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin Teste',
+            'email' => 'admin@projeto.com',
+            'password' => bcrypt('password'),
+            'email_verified_at' => now(), 
+        ]);
+
+        User::factory(5)->create();
+    }
+}
