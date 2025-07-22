db72b0e3e091c7308aa3aaea062c741dd8d217f5
Willian Michel <willian.tessarolo@itmapps.com.br>
2025-07-21 21:07:03 -0300

    fix: Ajustes na página inicial.

--- project-manager/resources/js/Layouts/GuestLayout.vue
+++ project-manager/resources/js/Layouts/GuestLayout.vue
@@ -1,12 +1,18 @@
 <script setup>
-import ApplicationLogo from '@/Components/ApplicationLogo.vue';
-import { Link } from '@inertiajs/vue3';
+import ApplicationLogo from "@/Components/ApplicationLogo.vue";
+import { Link } from "@inertiajs/vue3";
 </script>
 
 <template>
     <div
         class="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0 dark:bg-gray-900"
     >
+        <img
+            id="background"
+            class="absolute -left-20 top-0 max-w-[600px]"
+            src="https://laravel.com/assets/img/welcome/background.svg"
+            onerror="document.getElementById('background')?.classList.add('!hidden');"
+        />
         <div>
             <Link href="/">
                 <ApplicationLogo class="h-20 w-20 fill-current text-gray-500" />
